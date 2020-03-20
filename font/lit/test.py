27be72c9432cc310313ae29279843340e0da88f9
masknugget <54693275+masknugget@users.noreply.github.com>
2020-03-20 18:59:29 +0800

    Create test.py

---
+++ font/lit/test.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import requests
+import pandas as pd
+
+class Water:
+    def market(self):
+        ...
+
+    def strategy1(self):
+        ...
+
+    def strategy2(self):
+        ...
+
+    def volatility(self):
+        ...
+
+
+class Sidebar:
+    def ui_1(self):
+        ...
+
+    def work(self):
+        ...
+
+
+
+class Major:
+    def ui_1(self):
+        ...
+
+    def work(self):
+        ...
+
+
+
+class Publish:
+    def work(self):
+        ...
+
+
+
+
+
